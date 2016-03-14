c0eb69b8c35eeb777772708bcd842d437a508f4a
Perry.Zhang <zhangxiaohu@bestpay.com.cn>
2016-03-14 17:30:46 +0800

    zhangxiaohu add .gitignore file

--- frontpage/application/controllers/api/Api_user_info.php
+++ frontpage/application/controllers/api/Api_user_info.php
@@ -5,7 +5,7 @@ require APPPATH.'/libraries/REST_Controller.php';
  * @Author: Perry.Zhang
  * @Date:   2016-03-14 11:50:41
  * @Last Modified by:   Perry.Zhang
- * @Last Modified time: 2016-03-14 17:00:20
+ * @Last Modified time: 2016-03-14 17:13:05
  */
 
 class Api_user_info extends REST_Controller
